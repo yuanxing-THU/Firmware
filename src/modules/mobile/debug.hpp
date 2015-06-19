3c629ad4eed7e67f5cc51c1a583266a868a4e61b
Martins Frolovs <martins.f@airdog.com>
2015-06-19 10:53:57 +0300

    [mobile] module ported from emulation.

---
+++ src/modules/mobile/debug.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cerrno>
+#include <cstring>
+
+#include <dog_debug.hpp>
+
+#define dbg(...) DOG_PRINT(__VA_ARGS__)
+#define dbg_perror(fmt, ...) \
+		dbg(fmt ": %i: %s\n", ##__VA_ARGS__, errno, strerror(errno));
