liuyuisanai/Theia-Pix

Show airdog error on leash.

src/modules/mavlink/mavlink_messages.cpp
@@ -2284,13 +2284,15 @@ protected:
 		get_mavlink_mode_state(&status, &pos_sp_triplet, &msg.HRT_system_status, &msg.HRT_base_mode, &msg.HRT_custom_mode);
 		msg.HRT_type = mavlink_system.type;
 		msg.HRT_autopilot = MAV_AUTOPILOT_PX4;
-                msg.HRT_mavlink_version = MAVLINK_VERSION;
+		msg.HRT_mavlink_version = MAVLINK_VERSION;
+		msg.HRT_error_code = status.error_code;
+		msg.HRT_error_id = status.error_id;
 
 		bool updated = _pos_sub->update(&_pos_time, &pos);
 		updated |= _home_sub->update(&_home_time, &home);
 		updated |= _gps_sub->update(&_gps_time, &gps);
 
-                msg.STS_battery_remaining = (uint8_t)(100.0f * status.battery_remaining);
+		msg.STS_battery_remaining = (uint8_t)(100.0f * status.battery_remaining);
 
 		if (_pos_time != 0 && updated) {
 			msg.fresh_messages |= MAVLINK_COMBO_MESSAGE_GPOS;
@@ -2343,17 +2345,16 @@ protected:
 			}
 		}
 
-
 #if defined(CONFIG_IS_AIRDOG_FMU) || defined(CONFIG_IS_PX4_FMU)
-                // only airdog should send to leash calibration status
-                if (_calibrator_sub->update(&_calibrator_time, &calibrator)) {
-                    msg.fresh_messages |= MAVLINK_COMBO_MESSAGE_CALIBRATOR;
-                    msg.HRT_calibration_status = calibrator.status;
-                    msg.HRT_calibration_error = (int8_t)calibrator.result;
-                }
+        // only airdog should send to leash calibration status
+        if (_calibrator_sub->update(&_calibrator_time, &calibrator)) {
+            msg.fresh_messages |= MAVLINK_COMBO_MESSAGE_CALIBRATOR;
+            msg.HRT_calibration_status = calibrator.status;
+            msg.HRT_calibration_error = (int8_t)calibrator.result;
+        }
 #endif
 
-                if (msg.fresh_messages != 0) {
+		if (msg.fresh_messages != 0) {
 			_mavlink->send_message(MAVLINK_MSG_ID_HRT_GPOS_TRAJ_COMMAND, &msg);
 		}
 	}

src/modules/mavlink/mavlink_receiver.cpp
@@ -1129,7 +1129,7 @@ MavlinkReceiver::handle_combo_message(mavlink_message_t *msg)
 			msg_combo.HRT_mavlink_version
 		};
 
-                received_mavlink_version = msg_combo.HRT_mavlink_version;
+		received_mavlink_version = msg_combo.HRT_mavlink_version;
 
 //		msg_hrt.autopilot = msg_combo.HRT_autopilot;
 //		msg_hrt.base_mode = msg_combo.HRT_base_mode;
@@ -1138,10 +1138,11 @@ MavlinkReceiver::handle_combo_message(mavlink_message_t *msg)
 //		msg_hrt.system_status = msg_combo.HRT_system_status;
 //		msg_hrt.type = msg_combo.HRT_type;
 
-                // save additional airdog status field
-                // publishing will happen in internal_heartbeat_handle
-                _airdog_status.battery_remaining = msg_combo.STS_battery_remaining;
-
+        // save additional airdog status field
+        // publishing will happen in internal_heartbeat_handle
+        _airdog_status.battery_remaining = msg_combo.STS_battery_remaining;
+        _airdog_status.error_code = msg_combo.HRT_error_code;
+        _airdog_status.error_id = msg_combo.HRT_error_id;
 		internal_heartbeat_handle(msg_hrt, msg);
 	}
 	if (msg_combo.fresh_messages & MAVLINK_COMBO_MESSAGE_GPOS) {

src/modules/uORB/topics/airdog_status.h
@@ -55,6 +55,8 @@ struct airdog_status_s {
 	uint16_t discharged_mah;
 	uint8_t state_main;     ///< AirDog-FMU's vehicle_status main_state
 	uint8_t state_aird;     ///< AirDog-FMU's vehicle_status airdog_state
+	uint8_t error_code;
+	uint8_t error_id;
  }; /**< command sent to vehicle */
 
 /**

src/modules/uORB/topics/vehicle_status.h
@@ -276,6 +276,9 @@ struct vehicle_status_s {
 
 	bool auto_takeoff_cmd;
 	bool freeze_cmd; //commands loiter mode to freeze all movements and yawing
+
+	uint8_t error_code;
+	uint8_t error_id;
 };
 
 /**

yuanxing-THU/Firmware

src/modules/mavlink/mavlink_messages.cpp
@@ -2284,13 +2284,15 @@ protected:
 		get_mavlink_mode_state(&status, &pos_sp_triplet, &msg.HRT_system_status, &msg.HRT_base_mode, &msg.HRT_custom_mode);
 		msg.HRT_type = mavlink_system.type;
 		msg.HRT_autopilot = MAV_AUTOPILOT_PX4;
-                msg.HRT_mavlink_version = MAVLINK_VERSION;
+		msg.HRT_mavlink_version = MAVLINK_VERSION;
+		msg.HRT_error_code = status.error_code;
+		msg.HRT_error_stamp = status.error_stamp;
 
 		bool updated = _pos_sub->update(&_pos_time, &pos);
 		updated |= _home_sub->update(&_home_time, &home);
 		updated |= _gps_sub->update(&_gps_time, &gps);
 
-                msg.STS_battery_remaining = (uint8_t)(100.0f * status.battery_remaining);
+		msg.STS_battery_remaining = (uint8_t)(100.0f * status.battery_remaining);
 
 		if (_pos_time != 0 && updated) {
 			msg.fresh_messages |= MAVLINK_COMBO_MESSAGE_GPOS;
@@ -2343,17 +2345,16 @@ protected:
 			}
 		}
 
-
 #if defined(CONFIG_IS_AIRDOG_FMU) || defined(CONFIG_IS_PX4_FMU)
-                // only airdog should send to leash calibration status
-                if (_calibrator_sub->update(&_calibrator_time, &calibrator)) {
-                    msg.fresh_messages |= MAVLINK_COMBO_MESSAGE_CALIBRATOR;
-                    msg.HRT_calibration_status = calibrator.status;
-                    msg.HRT_calibration_error = (int8_t)calibrator.result;
-                }
+        // only airdog should send to leash calibration status
+        if (_calibrator_sub->update(&_calibrator_time, &calibrator)) {
+            msg.fresh_messages |= MAVLINK_COMBO_MESSAGE_CALIBRATOR;
+            msg.HRT_calibration_status = calibrator.status;
+            msg.HRT_calibration_error = (int8_t)calibrator.result;
+        }
 #endif
 
-                if (msg.fresh_messages != 0) {
+		if (msg.fresh_messages != 0) {
 			_mavlink->send_message(MAVLINK_MSG_ID_HRT_GPOS_TRAJ_COMMAND, &msg);
 		}
 	}

src/modules/mavlink/mavlink_receiver.cpp
@@ -1129,7 +1129,7 @@ MavlinkReceiver::handle_combo_message(mavlink_message_t *msg)
 			msg_combo.HRT_mavlink_version
 		};
 
-                received_mavlink_version = msg_combo.HRT_mavlink_version;
+		received_mavlink_version = msg_combo.HRT_mavlink_version;
 
 //		msg_hrt.autopilot = msg_combo.HRT_autopilot;
 //		msg_hrt.base_mode = msg_combo.HRT_base_mode;
@@ -1138,10 +1138,11 @@ MavlinkReceiver::handle_combo_message(mavlink_message_t *msg)
 //		msg_hrt.system_status = msg_combo.HRT_system_status;
 //		msg_hrt.type = msg_combo.HRT_type;
 
-                // save additional airdog status field
-                // publishing will happen in internal_heartbeat_handle
-                _airdog_status.battery_remaining = msg_combo.STS_battery_remaining;
-
+        // save additional airdog status field
+        // publishing will happen in internal_heartbeat_handle
+        _airdog_status.battery_remaining = msg_combo.STS_battery_remaining;
+        _airdog_status.error_code = msg_combo.HRT_error_code;
+        _airdog_status.error_stamp = msg_combo.HRT_error_stamp;
 		internal_heartbeat_handle(msg_hrt, msg);
 	}
 	if (msg_combo.fresh_messages & MAVLINK_COMBO_MESSAGE_GPOS) {

src/modules/uORB/topics/airdog_status.h
@@ -55,6 +55,8 @@ struct airdog_status_s {
 	uint16_t discharged_mah;
 	uint8_t state_main;     ///< AirDog-FMU's vehicle_status main_state
 	uint8_t state_aird;     ///< AirDog-FMU's vehicle_status airdog_state
+	uint8_t error_code;
+	uint8_t error_stamp;
  }; /**< command sent to vehicle */
 
 /**

src/modules/uORB/topics/vehicle_status.h
@@ -276,6 +276,9 @@ struct vehicle_status_s {
 
 	bool auto_takeoff_cmd;
 	bool freeze_cmd; //commands loiter mode to freeze all movements and yawing
+
+	uint8_t error_code;
+	uint8_t error_stamp;
 };
 
 /**