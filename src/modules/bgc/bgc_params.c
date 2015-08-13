c0631d7ba73e1c74397acaf89a301dcbfe2a5528
gints <gints@airdog.com>
2015-08-13 16:07:39 +0300

    [bgc] enable video tx power pin on boot for new hardware

--- src/modules/bgc/bgc_params.c
+++ src/modules/bgc/bgc_params.c
@@ -6,3 +6,9 @@
  * Any non-zero value = enable, 0 = disable.
  */
 PARAM_DEFINE_INT32(A_ARM_BGC_MOTORS, 1);
+
+/**
+ * Enable setting the Video Tx Power pin to 1 upon BGC initialization.
+ * 1 = enable, 0 = disable.
+ */
+PARAM_DEFINE_INT32(A_BGC_EN_VTX_POW, 1);
