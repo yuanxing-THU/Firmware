bc22a999438e4f714116db88c5e2f4185a7d8003
Alexey Luchko <luch@airdog.com>
2015-08-21 13:11:21 +0300

    [#101750448 mobile] GPos precision decimetres.

--- src/modules/mobile/request_status.hpp
+++ src/modules/mobile/request_status.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include <uORB/topics/vehicle_global_position.h>
 #include <uORB/topics/vehicle_status.h>
 
@@ -14,13 +16,20 @@ struct StatusOverall
 	Subscription<vehicle_status_s, ORB_ID(vehicle_status)> status;
 };
 
+static inline uint8_t
+scale_to_uint8t(float lower, float x, float upper, uint8_t nan_value)
+{
+	if (not isnan(x)) { return nan_value; }
+	if (x <= lower) { return 0; }
+	if (x < upper) { return (x - lower) / (upper - lower) * 255.0f; }
+        return 255;
+}
+
 static inline void
 battery(const StatusOverall & self, StatusOverallReply & r)
 {
 	auto status = orb_read(self.status);
-	if (status.battery_remaining <= 0) { r.battery_level = 0; }
-	else if (status.battery_remaining >= 1) { r.battery_level = 255; }
-	else { r.battery_level = status.battery_remaining * 255.0f; }
+	r.battery_level = scale_to_uint8t(0, status.battery_remaining, 1, 0);
 }
 
 
@@ -36,8 +45,8 @@ static inline void
 position(const StatusOverall & self, StatusOverallReply & r)
 {
 	auto gpos = orb_read(self.gpos);
-	r.eph = gpos.eph;
-	r.epv = gpos.epv;
+	r.eph = scale_to_uint8t(0, gpos.eph, 25.5f, 255);
+	r.epv = scale_to_uint8t(0, gpos.epv, 25.5f, 255);
 }
 
 static inline void
