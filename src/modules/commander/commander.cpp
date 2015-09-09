0d528c53b662a283d4b7bcf7ebec3ca10d26d40c
Anthony Kenga <anton.k@airdog.com>
2015-09-09 10:32:10 +0300

    [commander] Always try to hold position during land

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -3125,9 +3125,9 @@ set_control_mode()
 		control_mode.flag_control_auto_enabled = true;
 		control_mode.flag_control_rates_enabled = true;
 		control_mode.flag_control_attitude_enabled = true;
-		/* in failsafe LAND mode position may be not available */
-		control_mode.flag_control_position_enabled = status.condition_local_position_valid;
-		control_mode.flag_control_velocity_enabled = status.condition_local_position_valid;
+		/* if one wants to land without position use navigation state fallback */
+		control_mode.flag_control_position_enabled = true;
+		control_mode.flag_control_velocity_enabled = true;
 		control_mode.flag_control_altitude_enabled = true;
 		control_mode.flag_control_climb_rate_enabled = true;
 		control_mode.flag_control_termination_enabled = false;
