3a18ca3ec8896a2bbdd063f4c6c98d4555c2e8b9
Alexey Luchko <luch@airdog.com>
2015-09-10 20:25:40 +0300

    [sensors_switch] let it return error code.

--- src/modules/sensors_switch/main.cpp
+++ src/modules/sensors_switch/main.cpp
@@ -22,62 +22,76 @@ extern "C" __EXPORT int main(int argc, const char * const * const argv);
 namespace {
 
 inline bool
-streq(const char *a, const char *b) {
-	return not std::strcmp(a, b);
-}
+streq(const char *a, const char *b) { return not std::strcmp(a, b); }
 
-void
+bool
 report_system_voltage()
 {
 	int sub = orb_subscribe(ORB_ID(system_power));
+	if (sub == -1)
+	{
+		perror("orb_subscribe(system_power)");
+		return false;
+	}
+
 	system_power_s system_power;
 	orb_copy(ORB_ID(system_power), sub, &system_power);
 	orb_unsubscribe(sub);
+
 	printf("System voltage: %.2f\n", (double)system_power.voltage5V_v);
-	printf("Servo valid: %d, brick valid: %d, usb conncted: %d\n", system_power.servo_valid,
-		system_power.brick_valid, system_power.usb_connected);
+	printf("Servo valid: %d, brick valid: %d, usb conncted: %d.\n"
+		, system_power.servo_valid
+		, system_power.brick_valid
+		, system_power.usb_connected);
+
+	return true;
 }
 
-void
+bool
+read_adc(struct adc_msg_s (&adc)[N_ADC_CHANNELS])
+{
+	int f = open(ADC_DEVICE_PATH, O_RDONLY);
+
+	if (f == -1)
+	{
+		perror(ADC_DEVICE_PATH);
+		return false;
+	}
+
+	ssize_t r = read(f, &adc, sizeof adc);
+	if (r != sizeof adc) { perror(ADC_DEVICE_PATH); }
+
+	close(f);
+
+	return r == sizeof adc;
+}
+
+bool
 report_sensor_voltage()
 {
-	int fd_adc = open(ADC_DEVICE_PATH, O_RDONLY);
-
-	if (fd_adc < 0) {
-		fprintf(stderr, "Failed to open" ADC_DEVICE_PATH "\n");
-		return;
-	}
-
 	struct adc_msg_s adc[N_ADC_CHANNELS];
-	int ret, i;
-	ret = read(fd_adc, &adc, sizeof(adc));
-	if (ret < 0) {
-		perror(ADC_DEVICE_PATH " read");
-		fprintf(stderr, ADC_DEVICE_PATH " read failed.");
-	}
-	else
+	if (not read_adc(adc)) { return false; }
+
+	bool ok = false;
+	for (size_t i = 0; i < N_ADC_CHANNELS; ++i)
 	{
-		const int n = ret / sizeof(*adc);
-		bool ok = false;
-		for (i = 0; i < n; ++i) {
-			fprintf(stderr, "%i adc %i data %4u (0x%04x)\n",
-					i, adc[i].am_channel,
-					adc[i].am_data, adc[i].am_data);
+		fprintf(stderr, "%u adc %i data %4u (0x%04x)\n",
+				i, adc[i].am_channel,
+				adc[i].am_data, adc[i].am_data);
 #ifdef ADC_SENSORS_VOLTAGE_CHANNEL
-			if (adc[i].am_channel == ADC_SENSORS_VOLTAGE_CHANNEL)
-			{
-				printf("Sensor voltage: %u %.2f\n"
-					, adc[i].am_data
-					, adc[i].am_data * ADC_SENSORS_VOLTAGE_SCALE
-				);
-				ok = true;
-			}
-#endif
+		if (adc[i].am_channel == ADC_SENSORS_VOLTAGE_CHANNEL)
+		{
+			printf("Sensor voltage: %u %.2f\n"
+				, adc[i].am_data
+				, adc[i].am_data * ADC_SENSORS_VOLTAGE_SCALE
+			);
+			ok = true;
 		}
-		if (not ok)
-			fprintf(stderr, "Sensor voltage undetermined.\n");
+#endif
 	}
-	close(fd_adc);
+	if (not ok)
+		fprintf(stderr, "Sensor voltage undetermined.\n");
+	return ok;
 }
 
 } // end of namespace
@@ -90,29 +104,42 @@ main(int argc, const char * const * const argv)
 		fprintf(stderr,
 			"Usage: %s command [...]\n"
 			"\n"
-			"Command is one of: on, off, status, 10ms."
+			"Command is one of: on, off, status, 3ms."
 			" Each could be repeated multiple times\n"
 			, argv[0]);
+		return 1;
 	}
 
+	bool ok = false;
 	for (int i = 1; i < argc; ++i)
 	{
-		fprintf(stderr, "%s\n", argv[i]);
 		if (streq(argv[i], "on"))
+		{
+			ok = true;
+			printf("%s\n", argv[i]);
 			stm32_gpiowrite(SENSOR_SWITCH_GPIO, 1);
+		}
 		else if (streq(argv[i], "off"))
+		{
+			ok = true;
+			printf("%s\n", argv[i]);
 			stm32_gpiowrite(SENSOR_SWITCH_GPIO, 0);
+		}
 		else if (streq(argv[i], "status"))
 		{
 			bool on = stm32_gpioread(SENSOR_SWITCH_GPIO);
 			printf("Sensor power %s\n", on ? "on" : "off");
-			report_sensor_voltage();
-			report_system_voltage();
+			ok = report_sensor_voltage();
+			ok = report_system_voltage() and ok;
 		}
-		else if (streq(argv[i], "1ms"))
-			usleep(10000);
+		else if (streq(argv[i], "3ms"))
+		{
+			ok = true;
+			printf("%s\n", argv[i]);
+			usleep(3000);
+		}
 		else { fprintf(stderr, "Unknown command: \"%s\"\n", argv[i]); }
 	}
 
-	return 0;
+	return ok ? 0 : 1;
 }
