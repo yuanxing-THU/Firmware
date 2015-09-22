cb5917b8474ce58ded2dbf75b530c6bbbb9a14f7
ilya <ilya@airdog.com>
2015-09-22 09:10:00 +0200

    [fixup] Param defaults in code fro AirDog and AirLeash

--- src/modules/navigator/offset_follow_params.c
+++ src/modules/navigator/offset_follow_params.c
@@ -7,14 +7,14 @@
  * @unit m
  * @min 0.0
  */
-PARAM_DEFINE_FLOAT(OFF_DST_MIN, 0.0f);
+PARAM_DEFINE_FLOAT(OFF_DST_MIN, 7.0f);
 
 /**
  * Maximum offset length on XY plane in Offset follow modes
  * @unit m
- * @min 100.0
+ * @min 50.0
  */
-PARAM_DEFINE_FLOAT(OFF_DST_MAX, 100.0f);
+PARAM_DEFINE_FLOAT(OFF_DST_MAX, 50.0f);
 
 /**
  * Maximum offset rotation speed in Offset follow modes
@@ -35,7 +35,7 @@ PARAM_DEFINE_FLOAT(OFF_ANGL_ERR_T, 0.3f);
  * In FRONT FOLLOW offset rotation speed will be calculated as follows:
  * target_speed * OFF_ROT_SPD_R
  */
-PARAM_DEFINE_FLOAT(OFF_ROT_SPD_R, 1.0f);
+PARAM_DEFINE_FLOAT(OFF_ROT_SPD_R, 3.0f);
 
 /**
  * Offset rotation speed change command step in CIRCLE AROUND mode.
@@ -52,10 +52,10 @@ PARAM_DEFINE_FLOAT(OFF_FR_ADD_ANG, 0.0f);
  * Maximum delta between actual target to drone angle and sp angle
  * @unit rad
  */
-PARAM_DEFINE_FLOAT(OFF_MAX_SP_ANG_D, 0.5f);
+PARAM_DEFINE_FLOAT(OFF_MAX_SP_ANG_D, 2.0f);
 
 /**
  * Initial distance for offset follow modes
  * @unit m
  */
-PARAM_DEFINE_FLOAT(OFF_INTL_DST, 10.0f);
+PARAM_DEFINE_FLOAT(OFF_INTL_DST, 8.0f);
