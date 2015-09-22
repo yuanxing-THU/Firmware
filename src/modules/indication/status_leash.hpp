ca8624442a3041492cba4c3e900fa8d2c029025c
Max Shvetsov <max@airdog.com>
2015-09-23 01:08:09 +0200

    [indication] 3 levels of battery warning blinking and beeping
    removed not needed stuff from indication

--- src/modules/indication/status_leash.hpp
+++ src/modules/indication/status_leash.hpp
@@ -7,11 +7,15 @@
 #include <cstdint>
 #include <cstdio>
 #include <ctime>
+#include <fcntl.h> // open
 
 #include <board_leds.h>
 #include <drivers/drv_hrt.h>
+#include <drivers/drv_tone_alarm.h>
+#include <drivers/boards/AirLeash/common/board_leds.h>
 #include <systemlib/param/param.h>
 #include <modules/leash/kbd_defines.hpp>
+#include <modules/commander/commander_error.h>
 #include <uORB/topics/airdog_status.h>
 #include <uORB/topics/vehicle_status.h>
 #include <uORB/topics/leash_status.h>
@@ -29,8 +33,7 @@ static int leash_sub;
 static param_t param_use_blue_led;
 static int32_t use_blue_led;
 
-static bool pos_valid, link_valid, force_update;
-static bool loiter_mode;
+static bool link_valid, force_update;
 leash_status_s l_status;
 
 void
@@ -41,20 +44,35 @@ init()
     leash_sub = orb_subscribe(ORB_ID(leash_status));
 	param_use_blue_led = param_find("LEASH_USE_BLUE_L");
 	force_update = true;
-	pos_valid = false;
 	link_valid = false;
-	loiter_mode = false;
 	use_blue_led = -1;
 }
 
 void
 update(hrt_abstime now)
 {
+    static int _error_code = -1;
 	vehicle_status_s status;
 	orb_copy(ORB_ID(vehicle_status), status_sub, &status);
 
-	airdog_status_s airdog_status;
-	orb_copy(ORB_ID(airdog_status), airdog_sub, &airdog_status);
+    bool battery_error_updated = false;
+	bool changed_link_valid = false;
+    bool updated = false;
+    airdog_status_s airdog_status_data;
+    orb_check(airdog_sub, &updated);
+    if (updated)
+    {
+        orb_copy(ORB_ID(airdog_status), airdog_sub, &airdog_status_data);
+        if (_error_code != airdog_status_data.error_code)
+        {
+            _error_code = airdog_status_data.error_code;
+            battery_error_updated = true;
+        }
+        bool link_ok = airdog_status_data.timestamp > 0
+                and now - airdog_status_data.timestamp < 3000000;
+        changed_link_valid = link_valid != link_ok;
+        link_valid = link_ok;
+    }
 
 
 	bool led_updated = false;
@@ -64,50 +82,53 @@ update(hrt_abstime now)
     }
 
 
-	bool changed_pos_valid = pos_valid != status.condition_global_position_valid;
-	pos_valid = status.condition_global_position_valid;
 
-	bool link_ok = airdog_status.timestamp > 0
-			and now - airdog_status.timestamp < 3000000;
-	bool changed_link_valid = link_valid != link_ok;
-	link_valid = link_ok;
 
-	bool now_loiter = link_valid
-			and (airdog_status.state_main == MAIN_STATE_LOITER);
-	bool changed_mode = loiter_mode != now_loiter;
-	loiter_mode = now_loiter;
-	if (changed_mode)
-		fprintf(stderr, "State main %i %i\n", airdog_status.state_main, loiter_mode);
 
 	int32_t x;
 	param_get(param_use_blue_led, &x);
 	bool changed_use_blue_led = x != use_blue_led;
 	use_blue_led = x;
 
-	if (force_update or changed_use_blue_led
-	or changed_link_valid or changed_pos_valid or changed_mode or led_updated
-	) {
+	if (force_update or
+            changed_use_blue_led or
+            changed_link_valid or
+            led_updated or
+            battery_error_updated)
+    {
 		uint32_t pattern;
+        uint32_t tone_pattern;
 
 		if (use_blue_led) {
-			switch((ModeId) l_status.menu_mode) {
-			case ModeId::FLIGHT:
-				pattern = pos_valid ? 0x1000 : 0xFFFFe;
-				break;
-			case ModeId::FLIGHT_ALT:
-				pattern = pos_valid ? 0x5000 : 0xFFFFa;
-				break;
-			case ModeId::FLIGHT_CAM:
-				pattern = pos_valid ? 0x1500 : 0xFFFea;
-				break;
-			default:
-				pattern = pos_valid ? 0x80000000 : 0xFFFF7;
-				break;
+			switch(_error_code) {
+                case BSC_ERROR_BATTERY_RTH_NOTIFY:
+                case BSC_ERROR_BATTERY_LAND_NOTIFY:
+                    pattern = 0x1000;
+                    tone_pattern = TONE_BATTERY_WARNING_SLOW_TUNE;
+                    break;
+                case BSC_ERROR_BATTERY_RTH:
+                case BSC_ERROR_BATTERY_LAND:
+                    pattern = 0x5000;
+                    tone_pattern = TONE_BATTERY_WARNING_FAST_TUNE;
+                    break;
+                case BSC_ERROR_BATTERY_RTH_WO_BATT:
+                case BSC_ERROR_BATTERY_LAND_DEATH:
+                    pattern = 0xA;
+                    tone_pattern = TONE_BATTERY_WARNING_FAST_TUNE;
+                    break;
+                default:
+                    tone_pattern = TONE_STOP_TUNE;
+                    pattern = 0x0;
+                    break;
 			}
+            int beeper_fd = open(TONEALARM_DEVICE_PATH, O_RDONLY);
+            led_set_intensity(LED_RED,100);
+            ioctl(beeper_fd, TONE_SET_ALARM, tone_pattern);
 			leds::set_pattern_repeat(LED_STATUS, pattern);
 
 			if (link_valid)
-				pattern = loiter_mode ? 0xFF00FF00 : 0xFFFFFFFd;
+				pattern = 0xFFFFFFFd;
+//  0xFF00FF00
 			else
 				pattern = 0x80000000;
 
