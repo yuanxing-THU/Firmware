2e2c81f6843819d88c935e13185f39c74e05e5e2
Alexey Luchko <luch@airdog.com>
2015-09-23 18:45:55 +0300

    [mobile] allow compilation without GPIO_BL600 defines.

--- src/modules/mobile/bl600_gpio.hpp
+++ src/modules/mobile/bl600_gpio.hpp
@@ -12,28 +12,34 @@ namespace bl600
 void
 reset()
 {
+#ifdef GPIO_BL600_RESET
 	dbg("bl600 resetting...\n");
 	stm32_gpiowrite(GPIO_BL600_RESET, 0);
 	sleep(3);
 	stm32_gpiowrite(GPIO_BL600_RESET, 1);
 	sleep(3);
 	dbg("bl600 reset done...\n");
+#endif
 }
 
 void
 mode_AT()
 {
+#ifdef GPIO_BL600_RESET
 	stm32_gpiowrite(GPIO_BL600_SIO_07, 0);
 	stm32_gpiowrite(GPIO_BL600_SIO_28, 1);
 	reset();
+#endif
 }
 
 void
 mode_default()
 {
+#ifdef GPIO_BL600_RESET
 	stm32_configgpio(GPIO_BL600_SIO_07);
 	stm32_configgpio(GPIO_BL600_SIO_28);
 	reset();
+#endif
 }
 
 } // end of namespace bl600
