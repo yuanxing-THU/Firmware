2046b3f48829f7835bae50d9a104d3600a795b07
Alexey Luchko <luch@airdog.com>
2015-09-25 17:14:36 +0300

    [#102702262 factory] AirDogFMU: mobile dependency on sdlog2.

--- makefiles/config_Factory-AirDogFMU.mk
+++ makefiles/config_Factory-AirDogFMU.mk
@@ -13,6 +13,7 @@ MODULES += lib/activity
 MODULES += lib/airdog/hwinfo
 MODULES += lib/stm32f4
 MODULES += modules/mobile
+MODULES += modules/sdlog2
 
 ROMFS_EXTRA_FILES += $(PX4_BASE)Images/px4io-v2_default.bin
 ROMFS_EXTRA_FILES += $(wildcard $(PX4_BASE)ROMFS/factory/AirDogFMU/*)
